44aa90d00adaecab1c1f7633646d07993c164cf9
yegor <yegorveretelnik@gmail.com>
2025-07-20 18:01:25 +0200

    commit message

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import contactsRouter from './routes/contactsRouter.js';
+import contactsRouter from './routes/contacts.js';
 
 function setupServer() {
   const app = express();

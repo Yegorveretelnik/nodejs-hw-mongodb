23b380aedbc0cafde6d7d3e4ae452a6036b8cf03
yegor <yegorveretelnik@gmail.com>
2025-08-15 00:21:22 +0200

    commit message

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import dotenv from 'dotenv';
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  const { default: dotenv } = await import('dotenv');
+  dotenv.config();
+  console.log('[ENV] .env loaded (dev)');
+} else {
+  console.log('[ENV] production env (Render panel)');
+}
 
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
